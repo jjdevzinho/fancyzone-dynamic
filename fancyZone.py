87ca2ec16c4674cc222da4235264232ea4e6d891
jjdevzinho <jjdevzinho@gmail.com>
2024-12-28 01:42:07 -0300

    refatorando codigo

--- fancyZone.py
+++ fancyZone.py
@@ -19,35 +19,37 @@ def save_json(file_path, data):
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
+def modify_layout(columns_percentage):
+    data = load_json(layout_file)
+    layout = data["custom-layouts"][0]  # Acessa o primeiro (e único) layout
+    if layout["name"] == "dynamic":
+        layout["info"]["columns-percentage"] = columns_percentage
+        save_json(layout_file, data)
+        pyautogui.hotkey('ctrl', 'win', 'alt', '1')
+
 def modify_zone(percent_change):
     data = load_json(layout_file)
-    for layout in data["custom-layouts"]:
-        if layout["name"] == "dynamic":
-            columns_percentage = layout["info"]["columns-percentage"]
-            
-            col1 = columns_percentage[0] + percent_change * 100
-            col2 = columns_percentage[1] - percent_change * 100
+    layout = data["custom-layouts"][0]  # Acessa o primeiro (e único) layout
+    if layout["name"] == "dynamic":
+        columns_percentage = layout["info"]["columns-percentage"]
+        
+        col1 = columns_percentage[0] + percent_change * 100
+        col2 = columns_percentage[1] - percent_change * 100
 
-            col1 = max(0, min(10000, col1))
-            col2 = max(0, min(10000, col2))
+        col1 = max(0, min(10000, col1))
+        col2 = max(0, min(10000, col2))
 
-            layout["info"]["columns-percentage"] = [col1, col2]
-            save_json(layout_file, data)
-            pyautogui.hotkey('ctrl', 'win', 'alt', '1')
-            return
+        modify_layout([col1, col2])
 
 def invert_zones():
     data = load_json(layout_file)
-    for layout in data["custom-layouts"]:
-        if layout["name"] == "dynamic":
-            columns_percentage = layout["info"]["columns-percentage"]
-            
-            col1, col2 = columns_percentage[1], columns_percentage[0]
+    layout = data["custom-layouts"][0]  # Acessa o primeiro (e único) layout
+    if layout["name"] == "dynamic":
+        columns_percentage = layout["info"]["columns-percentage"]
+        
+        col1, col2 = columns_percentage[1], columns_percentage[0]
 
-            layout["info"]["columns-percentage"] = [col1, col2]
-            save_json(layout_file, data)
-            pyautogui.hotkey('ctrl', 'win', 'alt', '1')
-            return
+        modify_layout([col1, col2])
 
 def load_fancyzones_icon():
     if os.path.exists(fancyzones_icon_path):
